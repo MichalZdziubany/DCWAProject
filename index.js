02ba2b98597950f6afc33e1e7a8b3965ce44abb6
Michal <G00419292@atu.ie>
2024-12-30 15:26:00 +0000

    added routes in the home page

--- index.js
+++ index.js
@@ -2,10 +2,22 @@ const express = require('express');
 const app = express();
 const port = 3004;
 
-// Define a route
+//define routes for ejs files
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.render('home');
 });
+
+app.get('/students', (req, res) => {
+    res.render('students');
+});
+
+app.get('/grades', (req, res) => {
+    res.render('grades');
+});
+
+app.get('/lecturers', (req, res) => {
+    res.render('lecturers');
+  });
 
 // Start the server
 app.listen(port, () => {
